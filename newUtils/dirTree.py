f7a161efebc0d15317edc69b08615fc621940de4
Keith Sloan <keith@sloan-home.co.uk>
2023-01-27 19:57:36 +0000

    Add tree code

---
+++ newUtils/dirTree.py
@@ -0,0 +1,35 @@
+# https://realpython.com/directory-tree-generator-python/
+
+
+class _TreeGenerator:
+    def __init__(self, root_dir, dir_only=False):
+        self._root_dir = pathlib.Path(root_dir)
+        self._dir_only = dir_only
+        self._tree = []
+
+
+    def _tree_body(self, directory, prefix=""):
+        entries = self._prepare_entries(directory)
+        entries_count = len(entries)
+        for index, entry in enumerate(entries):
+            connector = ELBOW if index == entries_count - 1 else TEE
+            if entry.is_dir():
+                self._add_directory(
+                    entry, index, entries_count, prefix, connector
+                )
+            else:
+                self._add_file(entry, prefix, connector)
+
+
+    def _prepare_entries(self, directory):
+        entries = directory.iterdir()
+        if self._dir_only:
+            entries = [entry for entry in entries if entry.is_dir()]
+            return entries
+        entries = sorted(entries, key=lambda entry: entry.is_file())
+        return entries
+
+
+class DirectoryTree:
+    def __init__(self, root_dir, dir_only=False):
+        self._generator = _TreeGenerator(root_dir, dir_only)
